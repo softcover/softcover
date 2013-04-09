c0e71d45a6be4a6eab9566bb1caea806b06601e9
Michael Hartl <michael@michaelhartl.com>
2013-04-09 16:06:03 -0700

    [#47752169] Add a mimetype file

--- lib/polytexnic/builders/epub.rb
+++ lib/polytexnic/builders/epub.rb
@@ -3,7 +3,30 @@ module Polytexnic
     class Epub < Builder
 
       def build!
-        puts 'building epub!'
+        Dir.mkdir('epub') unless File.directory?('epub')
+        Dir.mkdir('epub/OEBPS') unless File.directory?('epub/OEBPS')
+        Dir.mkdir('epub/META-INF') unless File.directory?('epub/META-INF')
+        File.open('epub/mimetype', 'w') { |f| f.write('application/epub+zip') }
+        # raise manifest.filename
+      end
+
+      def template(title, content)
+        %(<?xml version="1.0" encoding="utf-8"?>
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+          "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+          <title>#{title}</title>
+          <link rel="stylesheet" href="styles/pygments.css" type="text/css" />
+          <link rel="stylesheet" href="styles/polytexnic.css" type="text/css" />
+          <link rel="stylesheet" type="application/vnd.adobe-page-template+xml" href="styles/page-template.xpgt" />
+        </head>
+
+        <body>
+          #{content}
+        </body>
+        </html>)
       end
 
     end
--- spec/builders/epub_spec.rb
+++ spec/builders/epub_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Polytexnic::Builders::Pdf do
+describe Polytexnic::Builders::Epub do
   context "in valid TeX directory" do
     before do
       chdir_to_book
@@ -11,10 +11,22 @@ describe Polytexnic::Builders::Pdf do
       subject(:builder) { Polytexnic::Builders::Epub.new }
       before { builder.build! }
 
-      it "should be create a tmp LaTeX file" do
-        expect(true).to be_true
-        expect(Polytexnic::Utils.tmpify('book.tex')).to exist
+      describe "mimetype file" do
+        it "should exist in the right directory" do
+          expect('epub/mimetype').to exist
+        end
+
+        it "should have the right contents" do
+          File.open('epub/mimetype') do |f|
+            expect(f.read).to match(/application\/epub\+zip/)
+          end
+        end
       end
+
+      it "should be create an EPUB file" do
+        expect(true).to be_true
+      end
+
 
     end
   end
