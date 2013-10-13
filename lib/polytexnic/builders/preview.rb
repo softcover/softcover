441f9effe949e1ee294a673c4e00dbf1bcc533fb
Michael Hartl <michael@michaelhartl.com>
2013-10-12 18:59:48 -0700

    Detect presence of GhostScript
    [#58760016]

--- lib/polytexnic/builders/preview.rb
+++ lib/polytexnic/builders/preview.rb
@@ -5,19 +5,28 @@ module Polytexnic
       def build!
         # Polytexnic::Builders::Mobi.new.build!    # Builds EPUB as a side-effect
         Polytexnic::Builders::Pdf.new.build!
-        pdf_split
+        extract_pdf_pages
       end
 
       private
 
-        def pdf_split
+        # Extracts pages from the PDF using GhostScript.
+        # The page range is set by the `pdf_preview_page_range` parameter
+        # in book.yml so that authors can override the default range.
+        def extract_pdf_pages
           input  = File.join('ebooks', manifest.filename + '.pdf')
           output = input.sub('.pdf', '-preview.pdf')
           range  = manifest.pdf_preview_page_range.split('..').map(&:to_i)
-          cmd  = %(yes | gs -dBATCH -sOutputFile="#{output}")
+          cmd  = %(yes | #{ghostscript} -dBATCH -sOutputFile="#{output}")
           cmd += %( -dFirstPage=#{range.first} -dLastPage=#{range.last})
           cmd += %( -sDEVICE=pdfwrite "#{input}" >& /dev/null)
           execute cmd
+        end
+
+        def ghostscript
+          filename = `which gs`.chomp
+          message  = "Install GhostScript (should come with LaTeX)"
+          @ghostscript ||= executable(filename, message)
         end
     end
   end
