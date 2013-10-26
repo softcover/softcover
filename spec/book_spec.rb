52957ce820f52bacb3671b26a2c18db897d30919
Michael Hartl <michael@michaelhartl.com>
2013-10-26 12:27:37 -0700

    Finish making anal changes
    [Delivers #59602028]

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,27 +7,16 @@ describe Polytexnic::Book do
       after(:all)  { remove_book }
 
       # disabling these tests for now:
-      # its(:filenames) { should_not include "html/test-book.html"}
-      # its(:filenames) { should_not include "html/test-book_fragment.html"}
+      its(:filenames) { should_not include "html/test-book.html"}
 
-      # its(:filenames) { should include "html/chapter-1_fragment.html"}
-      # its(:filenames) { should_not include "html/chapter-1.html"}
+      its(:filenames) { should include "html/chapter-1_fragment.html"}
+      its(:filenames) { should_not include "html/chapter-1.html"}
 
-      # its(:filenames) { should include "book.mobi"}
-      # its(:filenames) { should include "book.epub"}
-      # its(:filenames) { should include "book.pdf"}
+      its(:filenames) { should include "ebooks/test-book.mobi"}
+      its(:filenames) { should include "ebooks/test-book.epub"}
+      its(:filenames) { should include "ebooks/test-book.pdf"}
 
       its(:slug) { should eq "book" }
     end
-
-    # context "valid md book directory" do
-    #   before { chdir_to_md_book }
-
-    #   its(:slug) { should eq "md-book" }
-    # end
-  end
-
-  context "#create" do
-
   end
 end
--- spec/webmock_helpers.rb
+++ spec/webmock_helpers.rb
@@ -126,10 +126,6 @@ module WebmockHelpers
     Dir.chdir File.join File.dirname(__FILE__), "fixtures/book"
   end
 
-  def chdir_to_md_book
-    Dir.chdir File.join File.dirname(__FILE__), "fixtures/md-book"
-  end
-
   def chdir_to_non_book
     Dir.chdir File.join File.dirname(__FILE__), "fixtures/non-book"
   end
@@ -146,10 +142,15 @@ module WebmockHelpers
     flags << '-m' if options[:markdown]
     flags << '-s' if options[:simple]
     silence { system "poly new #{name} #{flags.join(' ')}" }
-    # Polytexnic::Commands::Generator.generate_file_tree(name, options)
     chdir_to_book
+    File.mkdir 'html' unless File.exist?('html')
+    File.write(File.join('html', 'chapter-1.html'),          'test')
+    File.write(File.join('html', 'chapter-1_fragment.html'), 'test')
+    File.write(File.join('html', 'test_fragment.html'),      'test')
+    File.mkdir 'ebooks' unless File.exist?('ebooks')
     Polytexnic::FORMATS.each do |format|
-      File.open("test-book.#{format}", 'w') { |f| f.write('test') }
+      dir = format == 'html' ? 'html' : 'ebooks'
+      File.write(File.join(dir, "test-book.#{format}"), 'test')
     end
     Dir.mkdir("screencasts") unless File.directory?("screencasts")
     File.open(File.join('screencasts', 'ch1.mov'), 'w') { |f| f.write('test') }
