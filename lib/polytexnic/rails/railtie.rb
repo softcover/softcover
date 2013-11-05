81166c8f45d0fed311cb2644b0a18a988998559e
Nick Merwin <nick@lemurheavy.com>
2013-11-05 14:02:47 -0800

    railtie initializer use all groups; bumped version

--- lib/polytexnic/rails/railtie.rb
+++ lib/polytexnic/rails/railtie.rb
@@ -1,9 +1,9 @@
 module Polytexnic
   class Railtie < ::Rails::Railtie
-    initializer :assets do |config|
+    initializer "polytexnic", group: :all do |app|
       path = File.expand_path(File.join(File.dirname(__FILE__),
         "..", "template", "html", "stylesheets"))
-      Rails.application.config.assets.paths << path
+      app.config.assets.paths << path
     end
   end
 end
--- lib/polytexnic/version.rb
+++ lib/polytexnic/version.rb
@@ -1,3 +1,3 @@
 module Polytexnic
-  VERSION = "0.4.4"
+  VERSION = "0.4.5"
 end
