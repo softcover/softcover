9308a490ddce498f0cc108a6c9c2594a69ccb865
Michael Hartl <michael@michaelhartl.com>
2013-11-30 10:10:03 -0800

    Move the author to book.yml
    [Delivers #61331124]

--- spec/book_manifest_spec.rb
+++ spec/book_manifest_spec.rb
@@ -10,7 +10,7 @@ describe Softcover::BookManifest do
 
       describe "basic information" do
         its(:title) { should eq "book" }
-        its(:subtitle) { should eq "Change-me" }
+        its(:subtitle) { should eq "Change me" }
         its(:description) { should eq "Change me." }
         its(:cover) { should eq "images/cover.png" }
         its(:author) { should eq "Author Name" }
