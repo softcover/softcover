369d4fe2c906ef7d8e103bb9bc743ad1940d52b9
Michael Hartl <michael@michaelhartl.com>
2013-12-05 13:05:29 -0800

    Add code tag to sanitizer

--- lib/softcover/sanitizer.rb
+++ lib/softcover/sanitizer.rb
@@ -12,22 +12,23 @@ module Softcover
 
       sanitize_options = {
         elements: %w{div span p a ul ol li h1 h2 h3 h4
-          pre em sup table tbody thead tr td img},
+          pre em sup table tbody thead tr td img code},
         remove_contents: %w{script},
         attributes: {
           'div' => %w{id class data-tralics-id data-number data-chapter},
-          'a'   => %w{id class href},
-          'span'=> %w{id class style},
-          'ol'  => %w{id class},
-          'ul'  => %w{id class},
-          'li'  => %w{id class},
-          'sup' => %w{id class},
-          'h1'  => %w{id class},
-          'h2'  => %w{id class},
-          'h3'  => %w{id class},
-          'h4'  => %w{id class},
-          'img' => %w{id class src alt},
-          'em'  => %w{id class}
+          'a'    => %w{id class href},
+          'span' => %w{id class style},
+          'ol'   => %w{id class},
+          'ul'   => %w{id class},
+          'li'   => %w{id class},
+          'sup'  => %w{id class},
+          'h1'   => %w{id class},
+          'h2'   => %w{id class},
+          'h3'   => %w{id class},
+          'h4'   => %w{id class},
+          'img'  => %w{id class src alt},
+          'em'   => %w{id class},
+          'code' => %w{id class}
         },
         protocols: {
           'a'   => {'href' => [:relative, 'http', 'https', 'mailto']},
