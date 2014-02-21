ae4926d583765e711a0f0af2860d32d4088d49b0
Michael Hartl <michael@michaelhartl.com>
2014-02-20 17:48:20 -0800

    Update sanitizer for tables
    [#66028938]

--- lib/softcover/sanitizer.rb
+++ lib/softcover/sanitizer.rb
@@ -29,7 +29,11 @@ module Softcover
           'img'  => %w{id class src alt},
           'em'   => %w{id class},
           'code' => %w{id class},
-          'strong' => %w{id class}
+          'strong' => %w{id class},
+          'table'   => %w{id class},
+          'tbody'   => %w{id class},
+          'tr'   => %w{id class},
+          'td'   => %w{id class}
         },
         protocols: {
           'a'   => {'href' => [:relative, 'http', 'https', 'mailto']},
