55336dc4d95a8db5cd24dbebbd217ae760ab38d7
Nick Merwin <nick@lemurheavy.com>
2014-02-26 13:58:19 -0800

    added support for unpublish by slug option (-s slug)

--- lib/softcover/commands/publisher.rb
+++ lib/softcover/commands/publisher.rb
@@ -89,7 +89,26 @@ module Softcover::Commands::Publisher
     puts "Processed #{current_book.processed_screencasts.size} screencasts."
   end
 
-  def unpublish!
+  def unpublish!(slug=nil)
+    require "rest_client"
+    require "softcover/client"
+
+    if slug.present?
+      begin
+        res = Softcover::Client.new.destroy_book_by_slug(slug)
+        if res["errors"]
+          puts "Errors: #{res.errors}"
+          return false
+        else
+          puts "Done!"
+          return true
+        end
+      rescue RestClient::ResourceNotFound
+        puts "Book with slug='#{slug}' not found under this account."
+        return false
+      end
+    end
+
     return false unless current_book
     if current_book.destroy
       Softcover::BookConfig.remove
