b7a6e38c719836e4b1c3e6b2e3ef14f49c1ce72c
Michael Hartl <michael@michaelhartl.com>
2015-05-19 10:03:41 -0700

    Point to new book directory for the sake of softcover.css

--- lib/softcover/rails/railtie.rb
+++ lib/softcover/rails/railtie.rb
@@ -2,7 +2,7 @@ module Softcover
   class Railtie < ::Rails::Railtie
     initializer "softcover", group: :all do |app|
       path = File.expand_path(File.join(File.dirname(__FILE__),
-        "..", "template", "html", "stylesheets"))
+        "..", "book_template", "html", "stylesheets"))
       app.config.assets.paths << path
     end
   end
