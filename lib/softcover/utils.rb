216d41055b6f872ff4c72ceae5e16253495d042f
Jack Kinsella <jackkinsella@users.noreply.github.com>
2016-03-23 14:08:35 +0100

    softcover check compatible with brew
    Previously, epubcheck installed via Homebrew would fail the "softcover check" command.

--- lib/softcover/utils.rb
+++ lib/softcover/utils.rb
@@ -262,7 +262,7 @@ module Softcover::Utils
       # Finds EpubCheck anywhere on the path.
       version_3 = path('epubcheck-3.0/epubcheck-3.0.jar')
       version_4 = path('epubcheck-4.0.1/epubcheck.jar')
-      first_path(version_4) || first_path(version_3) || ""
+      first_path(version_4) || first_path(version_3) || get_filename(:'epubcheck') || ""
     when :inkscape
       default = '/Applications/Inkscape.app/Contents/Resources/bin/inkscape'
       filename_or_default(:inkscape, default)
